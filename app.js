5c9385edc50c45c81e5ead78a079671e8f6eb434
aayusharora <aayush113002@gmail.com>
2015-11-21 18:48:44 +0530

    app added

---
+++ app.js
@@ -0,0 +1,30 @@
+var health=angular.module('health',[])
+health.controller('healthcontroller',function($scope,$http){
+	 setTimeout(function(){
+  $http.get("http://localhost/hospitalp/tojson.php")
+  .success(function (data) {
+	
+		$scope.data = data;
+  console.log($scope.data)
+ 
+  	  console.log(data[0].sreport);
+    console.log(data[1].sreport);
+  for(i=0;i<data.length;i++){
+  	if(data[i].sreport==""){
+  		 $scope.sreport="Pending";
+  	}
+  	
+  	console.log(data[i].sreport);
+  	if(data[i].sreport!=""){
+	  	$scope.sreport=data[i].sreport;
+	  	 	console.log($scope.sreport);
+	
+	  }
+	  
+	  console.log($scope.sreport);
+  }
+  
+   });
+},5000);
+
+})
